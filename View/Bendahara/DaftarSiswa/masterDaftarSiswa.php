984d63da95ce19d4067fa26923ccea42f9b510c1
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-28 00:22:39 +0700

    Update masterDaftarSiswa.php

--- View/Bendahara/DaftarSiswa/masterDaftarSiswa.php
+++ View/Bendahara/DaftarSiswa/masterDaftarSiswa.php
@@ -4,14 +4,14 @@
 $ID = @$_GET['ID'];
 if(@$_GET['k'] == '') {
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Daftar Siswa');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include"Controller/Bendahara/DaftarSiswa/daftarSiswaQuery.php";
     include"tabelDaftarSiswa2.php";
     include"Controller/Other/daftarJurusanQuery.php";
     include"AddSiswa/modalAddSiswa.php";
 }elseif($_GET['k'] == 'Profile'){
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Profil Siswa');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include "Controller/Bendahara/DaftarSiswa/ProfilSiswa/profilSiswaQuery.php";
     $profil = mysqli_fetch_array($profilSiswaQuery); 
     $date = date("Y-m-d");
@@ -31,7 +31,7 @@ if(@$_GET['k'] == '') {
     }
 }elseif ($_GET['k'] == 'StatusPrint') {
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Print Status Pembayaran Siswa');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include "Controller/Bendahara/PengaturanData/DataSekolah/profilSekolahQuery.php";
     include "Controller/Bendahara/DaftarSiswa/ProfilSiswa/profilSiswaQuery.php";
     $profil = mysqli_fetch_array($profilSiswaQuery); 
@@ -54,7 +54,7 @@ if(@$_GET['k'] == '') {
     }
 }elseif ($_GET['k'] == 'HistoryPrint') {
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Print Riwayat Transaksi');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include "Controller/Bendahara/PengaturanData/DataSekolah/profilSekolahQuery.php";
     include "Controller/Bendahara/DaftarSiswa/ProfilSiswa/profilSiswaQuery.php";
     $profil = $profilSiswaQuery->fetch_array();
@@ -77,7 +77,7 @@ if(@$_GET['k'] == '') {
     }
 }elseif($_GET['k'] == 'DebtPrint'){
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Print Rekapitulasi Tunggakan');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include "Controller/Bendahara/PengaturanData/DataSekolah/profilSekolahQuery.php";
     include "Controller/Bendahara/DaftarSiswa/ProfilSiswa/profilSiswaQuery.php";
     $profil = $profilSiswaQuery->fetch_array();
@@ -100,7 +100,7 @@ if(@$_GET['k'] == '') {
     }
 }elseif($_GET['k'] == 'StudentImport'){
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Import Siswa');
-    include "View/Other/loading.php";
+    include "View/Other/Loading.php";
     include"ImportSiswa/importSiswa2.php";
 }elseif($_GET['k'] == 'ClassManagement'){
     logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Manajemen Kelas');
