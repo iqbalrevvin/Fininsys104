84881115cffb057386d3ec3e416ce8a974820d7d
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-28 01:07:43 +0700

    Update Bug Fix

--- View/Bendahara/PengaturanKeuangan/JenisKasKeluar/masterJenisKasKlr.php
+++ View/Bendahara/PengaturanKeuangan/JenisKasKeluar/masterJenisKasKlr.php
@@ -1,8 +1,8 @@
 <?php 
 if(@$_GET['k']==''){
 	logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Pengaturan Jenis Kas Keluar');
-	include "Config/configdb.php";
-	include "COntroller/Bendahara/PengaturanKeuangan/JenisKasKeluar/tabelJenisKasKlr.php";	
+	include "Config/ConfigDB.php";
+	include "Controller/Bendahara/PengaturanKeuangan/JenisKasKeluar/tabelJenisKasKlr.php";	
 	include "daftarJenisKasKlr.php";
 	include "modal_addJenisKasKlr.php";
 }
--- View/Bendahara/PengaturanKeuangan/JenisKasMasuk/masterJenisKasMsk.php
+++ View/Bendahara/PengaturanKeuangan/JenisKasMasuk/masterJenisKasMsk.php
@@ -1,7 +1,7 @@
 <?php 
 if(@$_GET['k']==''){
 	logAct($idUsers, $level, $namaTmpln, $tglAct, $jamAct, $tglJamAct, $mac, 'Navigasi Ke Halaman Pengaturan Jenis Kas Masuk');
-	include "Config/configdb.php";
+	include "Config/ConfigDB.php";
 	include "Controller/Bendahara/PengaturanKeuangan/JenisKasMasuk/tabelJenisKasMsk.php";	
 	include "daftarJenisKasMsk.php";
 	include "modal_addJenisKasMsk.php";
