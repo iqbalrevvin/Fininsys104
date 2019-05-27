9575e2f57c6155dac9b9619c97d5e510aa13dbbd
iqbalrevvin <iqbal.revvin@gmail.com>
2019-05-28 01:31:17 +0700

    Update showKelas.php

--- View/Bendahara/PengaturanData/DataKelas/showKelas.php
+++ View/Bendahara/PengaturanData/DataKelas/showKelas.php
@@ -1,5 +1,5 @@
 <?php
-	include "../../../../Config/configdb.php";
+	include "../../../../Config/ConfigDB.php";
 	include "../../../../Controller/Bendahara/PengaturanData/DataKelas/daftarKelasQuery.php";
 	include "../../../../Controller/Bendahara/PengaturanData/DataSekolah/profilSekolahQuery.php";
 	include "tabelDaftarKelas.php";
